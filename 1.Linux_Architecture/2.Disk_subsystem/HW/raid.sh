bc04b0d7ccce4ac03c947c6dcf0c312888aca563
Leonid Albrekht <lalbrekht@gmail.com>
2018-04-29 14:51:30 +0500

    Привёл скрипт в порядок. Сейчас всё делается в циклах

--- 1.Linux_Architecture/2.Disk_subsystem/HW/raid.sh
+++ 1.Linux_Architecture/2.Disk_subsystem/HW/raid.sh
@@ -5,8 +5,10 @@
 echo "Start creating RAID"
 echo " "
 
+# Создаем RAID 
 mdadm --create /dev/md0 -l 6 -n 5 /dev/sd{c,d,e,f,g}
 
+# Проверяем успешно ли создался
 mdadm -D /dev/md0
 if [ $? == 0 ]
 then
@@ -31,19 +33,20 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | gdisk /dev/md0
 EOF
 done
 
-mkfs.ext4 /dev/md0p1
-mkfs.ext4 /dev/md0p2
-mkfs.ext4 /dev/md0p3
-mkfs.ext4 /dev/md0p4
-mkfs.ext4 /dev/md0p5
+# Создаём файловые системы на предоставленных девайсах
+for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
 
+# Создаём папки куда будем монтировать
 mkdir /u0{1,2,3,4,5}
-echo -e `sudo blkid | grep md0 | awk '/md0p1/ {print $2}'` /u01 ext4 defaults 0 0 >> /etc/fstab
-echo -e `sudo blkid | grep md0 | awk '/md0p2/ {print $2}'` /u02 ext4 defaults 0 0 >> /etc/fstab
-echo -e `sudo blkid | grep md0 | awk '/md0p3/ {print $2}'` /u03 ext4 defaults 0 0 >> /etc/fstab
-echo -e `sudo blkid | grep md0 | awk '/md0p4/ {print $2}'` /u04 ext4 defaults 0 0 >> /etc/fstab
-echo -e `sudo blkid | grep md0 | awk '/md0p5/ {print $2}'` /u05 ext4 defaults 0 0 >> /etc/fstab
 
+# Прописываем в /etc/fstab
+echo "Our new devices" >> /etc/fstab
+for i in $(seq 1 5)
+do 
+    echo `sudo blkid /dev/md0p$i | awk '{print $2}'` /u0$i ext4 defaults 0 0 >> /etc/fstab
+done
+
+# Монитруем по папкам
 mount -a
 
 echo " "
